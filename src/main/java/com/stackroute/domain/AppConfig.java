785443df7324e9b3286afbad74624faa3f2fe512
santhosh-gv <santhosh2596@gmail.com>
2019-10-04 12:50:44 +0530

    task 2 commit

--- src/main/java/com/stackroute/domain/AppConfig.java
+++ src/main/java/com/stackroute/domain/AppConfig.java
@@ -2,10 +2,11 @@ package com.stackroute.domain;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
-
+    @Scope("prototype")
     @Bean(name = {"arjun reddy", "taxiwaala"})
     public Movie getMovie()
     {
